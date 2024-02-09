a86c732162319ab1da6dfa3af7b37a5d26f6676a
Leonardo Negrao <leonardolnegrao@gmail.com>
2024-02-09 15:23:56 -0300

    fix: update schemas and services

--- src/modules/book/book.service.ts
+++ src/modules/book/book.service.ts
@@ -8,31 +8,43 @@ import { env } from "../../utils/env";
 
 import { CreateBookInput, UploadBookInput } from "./book.schema";
 
-export async function createSignedUrl(input: UploadBookInput) {
+// Generate signed URLs for the book file and cover image
+export async function createSignedUrls(input: UploadBookInput) {
   const { userId, name } = input;
   
   // TODO: verify userId
 
-  const signedUrl = await getSignedUrl(
+  const bookSignedUrl = await getSignedUrl(
     r2,
     new PutObjectCommand({
       Bucket: env.R2_BUCKET,
-      Key: `${userId}/${name}-${randomUUID()}.epub`,
+      Key: `${userId}/books/${name}-${randomUUID()}.epub`,
       ContentType: "application/epub+zip",
     }),
     { expiresIn: 300 }
   );
 
-  return signedUrl;
+  const coverSignedUrl = await getSignedUrl(
+    r2,
+    new PutObjectCommand({
+      Bucket: env.R2_BUCKET,
+      Key: `${userId}/covers/${name}-${randomUUID()}.jpeg`,
+      ContentType: "image/jpeg",
+    }),
+    { expiresIn: 300 }
+  );
+
+  return { bookSignedUrl, coverSignedUrl };
 }
 
 export async function createBook(input: CreateBookInput) {
-  const { name: filename, signedUrl: url, ...rest } = input;
+  const { name: filename, bookSignedUrl: bookUrl, coverSignedUrl: coverUrl, ...rest } = input;
   const book = await prisma.book.create({
     data: {
       ...rest,
       filename,
-      url
+      bookUrl,
+      coverUrl
     }
   });
 
